49ef926d8f95dcbd0ab0016b4e57b4bef44841c0
Mikael Larsson <mikael@lundqvisttravaru.se>
2022-05-25 20:40:55 +0200

    Add fps counter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,29 @@ import SceneManager from './SceneManager';
 
 const canvas = document.querySelector("#c");
 let sceneManager = new SceneManager(canvas);
+let clock = new THREE.Clock();
+
 addLights();
 
 let chunk = new World(1024, 1024);
 chunk.display(sceneManager.scene);
+let frames = 0;
+let temp_frames = 0;
+let time = 0;
 
 function render()
 {
+    time += clock.getDelta();
+
+    if( time >= 1)
+    {
+        time = 0;
+        updateFps(frames);
+        frames = 0;
+    }
+
+    frames += 1;
+
     sceneManager.update();
     requestAnimationFrame(render);
 }
@@ -28,8 +44,13 @@ function addLights()
     dLight2.position.z = -1;
     sceneManager.scene.add(dLight);
     sceneManager.scene.add(dLight2);
-
 }
+
+function updateFps(frames)
+{
+    document.querySelector("#fps").innerHTML = frames;
+}
+
 
 render();
 
