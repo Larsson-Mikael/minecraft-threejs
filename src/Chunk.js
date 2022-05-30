d8d893f5bb3192da1769d1d180db77cf91917092
Mikael Larsson <mikael@lundqvisttravaru.se>
2022-05-30 20:50:30 +0200

    Add comments

--- src/Chunk.js
+++ src/Chunk.js
@@ -1,3 +1,4 @@
+import { Vector3 } from 'three';
 import config from './config';
 
 class Chunk 
@@ -9,6 +10,13 @@ class Chunk
         this.data = data;
     }
 
+    /**
+     * 3D to 1D index
+     * @param {*} x 
+     * @param {*} y 
+     * @param {*} z 
+     * @returns Index
+     */
     to1D(x, y, z)
     {
         let xMax = config.chunk.length;
@@ -16,14 +24,40 @@ class Chunk
         return (y * xMax * zMax) + (z * xMax) + x;
     }
 
+    /**
+     * Returns the normalized chunk position 
+     * @param {*} x
+     * @param {*} y 
+     * @param {*} z 
+     */
+    worldToChunkPosition(x, y ,z)
+    {
+        let nX = (x / config.chunk.length % 1) * config.chunk.length;
+        let nZ = (z / config.chunk.width % 1) * config.chunk.width;
+        return new Vector3(nX, y, nZ);
+    }
 
+
+    /**
+     * Set a block type at chunk position 
+     * @param {*} x 
+     * @param {*} y 
+     * @param {*} z 
+     * @param {*} type  - The block type
+     */
     setBlock(x, y, z, type)
     {
         let i = this.to1D(x, y, z);
         this.data[i] = type;
     }
 
-    // in chunk coordinates
+    /**
+     * Get block type from chunk position
+     * @param {*} x 
+     * @param {*} y 
+     * @param {*} z 
+     * @returns 
+     */
     getBlock(x, y, z)
     {
         let i = this.to1D(x, y, z);
